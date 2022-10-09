51697983ec5bf4b00d8023519efac8ba4de5bd93
Rosalia <99089959+RosaliaLey@users.noreply.github.com>
2022-10-08 20:46:36 -0400

    initial commit for blurry loading image or page

---
+++ README.md
@@ -0,0 +1,24 @@
+# Blurry Loading Image
+
+The image will start blurred at 0%. As we reach 100% the image will come into focus. This component could be used on other elements on the DOM, or to load-in your site going from 0-100.
+
+## Table of contents
+
+- [Screenshot](#screenshot)
+- [Links](#links)
+- [Built with](#built-with)
+
+### Screenshot
+
+![](img/blurry%20loading%20image.png)
+
+### Links
+
+- [Live Site URL](https://rosalialey.github.io/blurry-loading-image/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
---
+++ script.js
@@ -0,0 +1,24 @@
+const background = document.querySelector(".background");
+const loadText = document.querySelector(".loading-text");
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+// map a range of numbers to another range of number.
+// 0 to 100 and 1 to 0 for opacity, the blur we want will be 30
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
